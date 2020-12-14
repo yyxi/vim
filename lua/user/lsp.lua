4c0257ac8fae03602525db67b4fb0c705d996864
yyxi <178086254+yyxi@users.noreply.github.com>
2020-12-14 08:05:15 +0400

    feat: lua lsp configuration

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -2,7 +2,7 @@ local config = require'lspconfig'
 
 config.tsserver.setup{}
 config.rust_analyzer.setup{}
-config.sumneko_lua.setup{}
+-- config.sumneko_lua.setup{}
 config.terraformls.setup{}
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
@@ -13,3 +13,27 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     update_in_insert = false,
   }
 )
+
+require'lspconfig'.sumneko_lua.setup {
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          -- Setup your lua path
+          path = vim.split(package.path, ';'),
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = {
+            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+          },
+        },
+      },
+    },
+  }
