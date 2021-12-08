6b70335682d6b620500540bffd83180abafa1365
yyxi <178086254+yyxi@users.noreply.github.com>
2021-12-08 18:10:02 +0400

    fix: treesitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -17,11 +17,11 @@ require'nvim-treesitter.configs'.setup{
     incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = 'gnn',
-        node_incremental = 'grn',
-        scope_incremental = 'grc',
-        scope_decremental = 'grm',
-      }
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
     },
     textobjects = {
       move = {
