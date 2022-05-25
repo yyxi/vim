c4df62d8642f342c6c899817bee919e7a349f940
yyxi <178086254+yyxi@users.noreply.github.com>
2022-05-25 18:38:25 +0000

    fix: pick tree-sitter parsers

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,74 +1,115 @@
 require'nvim-treesitter.configs'.setup{
-    highlight = {
-      enable = true,
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true
+  },
+  matchup = {
+    enable = true,
+  },
+  autotag = {
+    enable = true,
+  },
+  playground = {
+    enable = true,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
     },
-    indent = {
-      enable = true
-    },
-    matchup = {
+  },
+  textobjects = {
+    move = {
       enable = true,
-    },
-    autotag = {
-      enable = true,
-    },
-    playground = {
-      enable = true,
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
+      set_jumps = true,
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
       },
     },
-    textobjects = {
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer",
-        },
-        goto_next_end = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
-        },
-        goto_previous_end = {
-          ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer",
-        },
-      },
-      select = {
-        enable = true,
-        lookahead = true,
+    select = {
+      enable = true,
+      lookahead = true,
 
-        keymaps = {
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['aC'] = '@class.outer',
-          ['iC'] = '@class.inner',
-          ['ac'] = '@conditional.outer',
-          ['ic'] = '@conditional.inner',
-          ['ae'] = '@block.outer',
-          ['ie'] = '@block.inner',
-          ['al'] = '@loop.outer',
-          ['il'] = '@loop.inner',
-          ['is'] = '@statement.inner',
-          ['as'] = '@statement.outer',
-          ['ad'] = '@comment.outer',
-          ['am'] = '@call.outer',
-          ['im'] = '@call.inner',
-        },
+      keymaps = {
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['aC'] = '@class.outer',
+        ['iC'] = '@class.inner',
+        ['ac'] = '@conditional.outer',
+        ['ic'] = '@conditional.inner',
+        ['ae'] = '@block.outer',
+        ['ie'] = '@block.inner',
+        ['al'] = '@loop.outer',
+        ['il'] = '@loop.inner',
+        ['is'] = '@statement.inner',
+        ['as'] = '@statement.outer',
+        ['ad'] = '@comment.outer',
+        ['am'] = '@call.outer',
+        ['im'] = '@call.inner',
       },
     },
-    ensure_installed = 'all',
-    lsp_interop = {
-      enable = true,
-    },
-  }
+  },
+  ensure_installed = {
+    "bash",
+    "bibtex",
+    "cmake",
+    "css",
+    "dockerfile",
+    "dot",
+    "eex",
+    "elixir",
+    "erlang",
+    "fish",
+    "glsl",
+    "go",
+    "graphql",
+    "hcl",
+    "heex",
+    "help",
+    "html",
+    "http",
+    "javascript",
+    "jsdoc",
+    "json",
+    "json5",
+    "jsonc",
+    "latex",
+    "lua",
+    "lua",
+    "make",
+    "markdown",
+    "perl",
+    "proto",
+    "python",
+    "r",
+    "rust",
+    "toml",
+    "tsx",
+    "typescript",
+    "vim",
+    "vue",
+    "wgsl",
+    "yaml",
+  },
+  lsp_interop = {
+    enable = true,
+  },
+}
